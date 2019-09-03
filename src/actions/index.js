25a94c9fca62e57df67a4787034acb9352fc2301
Mudassar "Moe" Bashir <moeb59@gmail.com>
2019-09-03 16:51:49 -0400

    187. Refactor back to older way of action creator (no memoization).

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonplaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -6,30 +5,7 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonplaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
-
-//---------------------------------------------------------
-// Old Syntax to help understand better the relationship between
-// fetchUser and _fetchUser.
-//---------------------------------------------------------
-
-// // This is now our action creator which is called by the component.
-// export const fetchUser = function(id) {
-//   console.log('fetchUser called with id ', id);
-//   return function(dispatch) {
-//     console.log('inner function called with id ', id);
-//     _fetchUser(id, dispatch);
-//   };
-// };
-//
-// // This is the memoized function called by the action creator.
-// const _fetchUser = _.memoize(async function(id, dispatch) {
-//   console.log('memoized function called with id ', id);
-//   const response = await jsonplaceholder.get(`/users/${id}`);
-//   dispatch({ type: 'FETCH_USER', payload: response.data });
-// });
+};
