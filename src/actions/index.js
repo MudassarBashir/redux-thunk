aed754457b4b0514c68d0f09ccc6bdf0b26677c5
Mudassar "Moe" Bashir <moeb59@gmail.com>
2019-09-05 05:46:55 -0400

    188. Action creators in action creators.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
 import jsonplaceholder from '../apis/jsonplaceholder';
 
+// Our components will now only call this action creator. Not the other
+// ones below.
+export const fetchPostsAndUsers = () => async dispatch => {
+  // Whenever we call an action creator from inside an action creator we must use
+  // dispatch like so. Await ensures we wait to get our posts before trying to use
+  // them.
+  await dispatch(fetchPosts());
+};
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonplaceholder.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
