8a6af783bc18b6895ba68be9110e9eea285effc6
Mudassar "Moe" Bashir <moeb59@gmail.com>
2019-09-06 06:16:43 -0400

    189. Finding unique user ids.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,15 @@
+import _ from 'lodash';
 import jsonplaceholder from '../apis/jsonplaceholder';
 
 // Our components will now only call this action creator. Not the other
 // ones below.
-export const fetchPostsAndUsers = () => async dispatch => {
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   // Whenever we call an action creator from inside an action creator we must use
   // dispatch like so. Await ensures we wait to get our posts before trying to use
   // them.
   await dispatch(fetchPosts());
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async dispatch => {
